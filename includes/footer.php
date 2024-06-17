9bbcdf840d7f13c53494dcd88c86088c8b203722
Muhammad Afrian <121986014+Manz2908@users.noreply.github.com>
2024-06-17 19:03:56 +0700

    f

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div class="footer-section footer-section1">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
